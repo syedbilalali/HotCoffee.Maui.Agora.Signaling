870689f223f0f446642ae5b22a891fe93b4acea8
syedbilalali <syedbilalali.dev@gmail.com>
2026-02-18 02:45:29 +0530

    feat(android): Add generic method to convert java object into C# System.Objects.

---
+++ src/HotCoffee.Maui.Agora.Signaling/JavaHolder.android.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotCoffee.Maui.Agora.Signaling
+{
+    public class JavaHolder : Java.Lang.Object
+    {
+        public readonly object Instance;
+
+        public JavaHolder(object instance)
+        {
+            Instance = instance;
+        }
+    }
+}
