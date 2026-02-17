8a5c2d9201b6ae41539dc353afb199b375758844
syedbilalali <syedbilalali.dev@gmail.com>
2026-02-18 02:50:01 +0530

    feat(maui): Consuming Maui function in Sample project to test the functionality in the app.

--- samples/HotCoffeeMauiAgoraSignalingSampleCsproj/App.xaml.cs
+++ samples/HotCoffeeMauiAgoraSignalingSampleCsproj/App.xaml.cs
@@ -6,12 +6,9 @@ namespace HotCoffeeMauiAgoraSignalingSampleCsproj
     public partial class App : Application
     {
         
-        public App(IRtmClient rtmClient)
+        public App()
         {
             InitializeComponent();
-            rtmClient.InitializeAsync(new() { AppId = "fkkfhkdhfkdfhfh374273283h23h2", UserId = "1213122" });
-            Debug.WriteLine(" Initialised " + rtmClient.ToString());
-
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- samples/HotCoffeeMauiAgoraSignalingSampleCsproj/MainPage.xaml.cs
+++ samples/HotCoffeeMauiAgoraSignalingSampleCsproj/MainPage.xaml.cs
@@ -1,28 +1,105 @@
 ﻿using HotCoffee.Maui.Agora.Signaling.Core.Abstractions;
+using HotCoffee.Maui.Agora.Signaling.Core.Models;
 using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace HotCoffeeMauiAgoraSignalingSampleCsproj
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private IRtmClient _tmClient;
 
-        public MainPage()
+
+        public static bool IsLoggedIn = false;
+
+        public MainPage(IRtmClient rtmClient)
         {
             InitializeComponent();
-           
+            rtmClient.InitializeAsync(new() { AppId = "124da2495f7d493494fe6e7c08184f6d", UserId = "bilal123" });
+            Debug.WriteLine(" Initialised " + rtmClient.ToString());
+            rtmClient.MessageReceived += RtmClient_MessageReceived;
+            _tmClient = rtmClient;
+
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void RtmClient_MessageReceived(object? sender, RtmMessage e)
         {
-            count++;
+            Console.WriteLine("------------> Messages Received : " + e.Payload.ToString());
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void RtmLogin_Clicked(object sender, EventArgs e)
+        {
+            _tmClient.LoginAsync("007eJxSYFgQcrzFTPDw4cYYQdGishc2/34JhfD9/Lui/aROUPTxfDcFBkMjk5REIxNL0zTzFBNLYxNLk7RUs1TzZAMLQwuTNLOU7yJTMhsCGRlMTfexMjEwMoAwiM8CJlkZMlJzcvI5GJIycxJzDI2MQdIQBQgxQAAAAP//AEcmag==", new RtmLoginResultCallBackSink());
+            
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void RtmLogout_Clicked(object sender, EventArgs e)
+        {
+            _tmClient.LogoutAsync(new RtmLogoutResultCallback());
+        }
+
+        private void RtmSendMessages_Clicked(object sender, EventArgs e)
+        {
+            _tmClient.SendMessageAsync("hello", "Self Messages ", new RtmSendMsgResultCallBackSink());
+        }
+
+        private void RtmSubscribe_Clicked(object sender, EventArgs e)
+        {
+            _tmClient.Subscribe("hello", new RtmSubscribeResultCallback());
+        }
+
+        public record RtmLogoutResultCallback : IRtmResultCallback
+        {
+            public void OnFailure(RtmError error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+
+            public void OnSuccess(object? responseInfo)
+            {
+                Console.WriteLine(responseInfo?.ToString());
+            }
+        }
+        public record RtmSubscribeResultCallback : IRtmResultCallback
+        {
+            public void OnFailure(RtmError error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+
+            public void OnSuccess(object? responseInfo)
+            {
+                Console.WriteLine(responseInfo?.ToString());
+            }
+        }
+
+        public record RtmLoginResultCallBackSink : IRtmResultCallback
+        {
+            public void OnFailure(RtmError error)
+            {
+                Console.WriteLine(error.ToString());
+                // Shell.Current.CurrentPage.DisplayAlert("Login", "Login failed !!!", "OK").ConfigureAwait(false);
+            }
+
+            public void OnSuccess(object? responseInfo)
+            {
+                IsLoggedIn = true;
+                Console.WriteLine(responseInfo?.ToString());
+                // Shell.Current.CurrentPage.DisplayAlert("Login", "Login Successfull !!!", "OK").ConfigureAwait(false);
+            }
+        }
+        public record RtmSendMsgResultCallBackSink : IRtmResultCallback
+        {
+            void IRtmResultCallback.OnFailure(RtmError error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+
+            void IRtmResultCallback.OnSuccess(object? responseInfo)
+            {
+                Console.WriteLine(responseInfo?.ToString());
+            }
         }
     }
+
 }
